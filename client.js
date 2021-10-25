469c046e686b00e6bbd05774364adb159168ef7a
LandonFerg <landonljf@gmail.com>
2021-10-24 23:07:51 -0400

    final

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ console.log("client code go here haha");
 var newDrinkButton = document.getElementById('testBtn');
 socket = io();
 newDrinkButton.addEventListener("click", function() {
-    var lightRum = getElementById('sl1').value;
+    var lightRum = document.getElementById('sl1').value;
     socket.emit('newDrink', lightRum);
 
     
