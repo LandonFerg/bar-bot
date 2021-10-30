947358c2e1b7308e700d99f40c28396dc6e0f0bb
LandonFerg <landonljf@gmail.com>
2021-10-30 17:05:45 -0400

    testing

--- app.js
+++ app.js
@@ -19,6 +19,28 @@ const pumpGPIO = [5,6,13,19];
 let pumps = []; // array to hold all our pumps
 initPumps(pumpGPIO); // initialize pumps for the gpio
 
+// Drink class
+class Drink {
+	constructor(name, d1, d2, d3, d4, d5, d6, m1, m2, m3, m4) {
+	  this.name = name;
+  
+	  // LIQUOR
+	  this.d1 = d1; // Light rum
+	  this.d2 = d2;
+	  this.d3 = d3;
+	  this.d4 = d4;
+	  this.d5 = d5;
+	  this.d6 = d6;
+  
+	  // MIXERS
+	  this.m1 = m1; // Coke
+	  this.m2 = m2;
+	  this.m3 = m3;
+	  this.m4 = m4;
+	}
+  }
+
+
 var testCount = 1;
 
 // Setup python-shell arguments
@@ -128,7 +150,7 @@ io.sockets.on('connection', function (socket) {
 
   drinks.push(createdDrink);
 
-  RefreshTable();
+  // RefreshTable(); cant use client jquery
 
   let newSavedDrink = {
 	  d1: drink1,
--- js/scripts.js
+++ js/scripts.js
@@ -7,27 +7,6 @@ var drinkVals = [];
 var mixerNames = ['Coke']
 var drinks = [];
 
-// Drink class
-class Drink {
-  constructor(name, d1, d2, d3, d4, d5, d6, m1, m2, m3, m4) {
-    this.name = name;
-
-    // LIQUOR
-    this.d1 = d1; // Light rum
-    this.d2 = d2;
-    this.d3 = d3;
-    this.d4 = d4;
-    this.d5 = d5;
-    this.d6 = d6;
-
-    // MIXERS
-    this.m1 = m1; // Coke
-    this.m2 = m2;
-    this.m3 = m3;
-    this.m4 = m4;
-  }
-}
-
 // let liquorNames = ["Light Rum", "Dark Rum", "Vodka", "Tequila", "Gin", "Triple Sec"];
 // let mxrNames = ["Coke", "Sprite", "Ginger Beer", "Margarita mix"];
 
